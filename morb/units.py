55a6d019795993d361e6e2f70d0bb511afb43671
Sander Dieleman <sanderdieleman@gmail.com>
2011-12-09 16:49:26 +0100

    license, todo update

--- morb/units.py
+++ morb/units.py
@@ -6,17 +6,15 @@ BinaryUnits = units_type(activation_functions.sigmoid, samplers.bernoulli_mf)
 SigmoidUnits = units_type(activation_functions.sigmoid, samplers.bernoulli_always_mf)
 
 GaussianUnits = units_type(activation_functions.identity, samplers.gaussian_always_mf)
+# TODO: this is always mf
+# also need a version that actually samples.
 
 SoftmaxUnits = units_type(activation_functions.softmax, samplers.multinomial)
 
-# TODO LATER: BetaUnits
-
-# TODO LATER: RELUUnits
-      
-class SymmetricBetaUnits(Units): # hoah
+class SymmetricBetaUnits(Units): # TODO
     # Symmetric because the hiddens switch between two beta distributions, not because
     # the parameters of the distribution are chosen to be equal (this is not the case).
     pass
     
-class ReLUUnits(Units): # hoah
+class ReLUUnits(Units): # TODO
     pass
