837f138d80c8db14a042d4d495c97cbf1ce780e6
Sander Dieleman <sanderdieleman@gmail.com>
2012-01-09 17:17:11 +0100

    import all submodules by default

---
+++ morb/__init__.py
@@ -0,0 +1,13 @@
+import base
+import parameters
+import units
+import rbms
+import stats
+import trainers
+import updaters
+import monitors
+import samplers
+import activation_functions
+
+__all__ = ['base', 'parameters', 'units', 'rbms', 'stats', 'trainers',
+           'updaters', 'monitors', 'samplers', 'activation_functions']
