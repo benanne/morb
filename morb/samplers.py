23498265c4b7f61136289d0518801e379a5b8e78
Sander Dieleman <sanderdieleman@gmail.com>
2012-03-14 21:14:35 +0100

    optimised gamma approximation a bit for GPU, thanks to nouiz

--- morb/samplers.py
+++ morb/samplers.py
@@ -56,6 +56,10 @@ def laplacian(b, mu=0.0):
     
     
 ## approximate gamma sampler
+# Two approximations for the gamma function are defined.
+# Windschitl is very fast, but problematic close to 0, and using the reflection formula
+# causes discontinuities.
+# Lanczos on the other hand is extremely accurate, but slower.
    
 def _log_gamma_windschitl(z):
     """
@@ -69,6 +73,56 @@ def _log_gamma_ratio_windschitl(z, k):
     """
     return _log_gamma_windschitl(z + k) - _log_gamma_windschitl(z)
     
+
+def _log_gamma_lanczos(z):
+    # optimised by nouiz. thanks!
+    assert z.dtype.startswith("float")
+    # reflection formula. Normally only used for negative arguments,
+    # but here it's also used for 0 < z < 0.5 to improve accuracy in
+    # this region.
+    flip_z = 1 - z
+    # because both paths are always executed (reflected and
+    # non-reflected), the reflection formula causes trouble when the
+    # input argument is larger than one.
+    # Note that for any z > 1, flip_z < 0.
+    # To prevent these problems, we simply set all flip_z < 0 to a
+    # 'dummy' value. This is not a problem, since these computations
+    # are useless anyway and are discarded by the T.switch at the end
+    # of the function.
+    flip_z = T.switch(flip_z < 0, 1, flip_z)
+    log_pi = np.asarray(np.log(np.pi), dtype=z.dtype)
+    small = log_pi - T.log(T.sin(np.pi * z)) - _log_gamma_lanczos_sub(flip_z)
+    big = _log_gamma_lanczos_sub(z)
+    return T.switch(z < 0.5, small, big)
+
+
+def _log_gamma_lanczos_sub(z): # expanded version
+    # optimised by nouiz. thanks!
+    # Coefficients used by the GNU Scientific Library
+    # note that vectorising this function and using .sum() turns out to be
+    # really slow! possibly because the dimension across which is summed is
+    # really small.
+    g = 7
+    p = np.array([0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+                  771.32342877765313, -176.61502916214059, 12.507343278686905,
+                  -0.13857109526572012, 9.9843695780195716e-6,
+                  1.5056327351493116e-7], dtype=z.dtype)
+    z = z - 1
+    x = p[0]
+    for i in range(1, g + 2):
+        x += p[i] / (z + i)
+    t = z + g + 0.5
+    pi = np.asarray(np.pi, dtype=z.dtype)
+    log_sqrt_2pi = np.asarray(np.log(np.sqrt(2 * np.pi)), dtype=z.dtype)
+    return log_sqrt_2pi + (z + 0.5) * T.log(t) - t + T.log(x)
+
+    
+def _log_gamma_ratio_lanczos(z, k):
+    """
+    computes log(gamma(z+k)/gamma(z)) using the lanczos approximation.
+    """ 
+    return _log_gamma_lanczos(z + k) - _log_gamma_lanczos(z)
+    
  
 def gamma_approx(k, theta=1):
     """
@@ -77,8 +131,8 @@ def gamma_approx(k, theta=1):
     computed on the GPU (using the Windschitl approximation).
     """
     lmbda = 1/3.0 # according to Wilson and Hilferty
-    mu = T.exp(_log_gamma_ratio_windschitl(k, lmbda))
-    sigma = T.sqrt(T.exp(_log_gamma_ratio_windschitl(k, 2*lmbda)) - mu**2)
+    mu = T.exp(_log_gamma_ratio_lanczos(k, lmbda))
+    sigma = T.sqrt(T.exp(_log_gamma_ratio_lanczos(k, 2*lmbda)) - mu**2)
     normal_samples = theano_rng.normal(size=k.shape, avg=mu, std=sigma, dtype=theano.config.floatX)
     gamma_samples = theta * T.abs_(normal_samples ** 3)
     # The T.abs_ is technically incorrect. The problem is that, without it, this formula may yield
